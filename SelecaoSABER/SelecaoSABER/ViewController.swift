e1efaa0399a23c6c1f83c2ab941bb25828dc45b7
Hilton Pintor <hpbl@cin.ufpe.br>
2017-08-26 07:00:07 -0300

    Adicionando Projeto Xcode

---
+++ SelecaoSABER/SelecaoSABER/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SelecaoSABER
+//
+//  Created by Hilton Pintor Bezerra Leite on 26/08/17.
+//  Copyright © 2017 Hilton Pintor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
